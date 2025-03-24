bc78d479c5f01429f5d1149aae56394c1e7535ba
AshishSingh1503 <23bec025@nith.ac.in>
2025-03-24 10:33:29 +0530

    update

--- hardware/rpi_master_slave/master.py
+++ hardware/rpi_master_slave/master.py
@@ -2,14 +2,14 @@ import socket
 import time
 import random
 
-SLAVE_IP = "192.168.8.126"  # Replace with your slave RPi's IP
+# SLAVE_IP =  # Replace with your slave RPi's IP
 PORT = 5005
 
 COMMANDS = ['L', 'R', 'M', 'S']
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-    client.connect((192.168.8.126, PORT))
-    print(f"Connected to {SLAVE_IP}:{PORT}")
+    client.connect(( "192.168.8.126", PORT))
+    print(f"Connected to :{PORT}")
 
     while True:
         cmd = random.choice(COMMANDS)  # Send a random command
